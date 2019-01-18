3cbb6c4591e65991dbf0b6e367036da7c5d48004
Luke MacRedmond <luke@lukes-mbp.nuigalway.ie>
2019-01-18 12:39:30 +0000

    Removed DualSensors code from Main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -21,57 +21,6 @@ void setup() {
   
 }
 
-void USRead(){
-  int averagingUS =0;
-
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-
-  digitalWrite(trigPin,HIGH);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, LOW);
-
-  
-  
-  //Loop for reading in values
-  for(int j=0; j<5; j++){
-
-    USdistance = (duration*0.034)/2;
-    averagingUS = averagingUS+ USdistance;
-
-  }
-  USdistance= averagingUS/5;
-  Serial.print("US Distance= ");
-  Serial.print(USdistance);
-  Serial.println(" cm  ");
-  delay(5);
-
-}
-
-void irread(){
-  int averaging =0;
-  int div = 10;
-  //Loop for reading in values
-  for(int i=0; i<10; i++){
-
-    //Read voltage on IRsense, A0 pin
-    IRdistance = analogRead(irsense);
-
-    //Summing current reading with previous
-    averaging= averaging + IRdistance;
-    Serial.print("Reading: ");
-    Serial.print(i);
-    Serial.println("  IR Distance");
-    delayMicroseconds(3000);
-
-  }
-  //
-  IRdistance = averaging/div;
-  Serial.print("Average IR distance: ");
-  Serial.println(IRdistance);
-  delayMicroseconds(1000);
-}
-
 void loop() {
 
 //USRead() -- was not declared in this scope
